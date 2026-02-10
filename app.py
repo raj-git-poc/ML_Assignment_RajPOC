90107cfb004787b29fe1d04dae937bdcc823ebbf
raj-git-poc <rajkumar.urs@gmail.com>
2026-02-11 01:51:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 st.title("Heart Disease Prediction - ML Models")
 
-uploaded_file = st.file_uploader("Upload Test CSV File", type=["csv"])
+uploaded_file = st.file_uploader("Heart_Disease_Prediction.csv", type=["csv"])
 
 model_name = st.selectbox(
     "Select Model",
