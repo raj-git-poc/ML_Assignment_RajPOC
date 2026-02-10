f969e5e470ba0c99e7d0a3d94c75c697a556a3e6
raj-git-poc <rajkumar.urs@gmail.com>
2026-02-11 03:56:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,25 @@ if uploaded_file is not None:
         X = data.drop("target", axis=1)
         y = data["target"]
 
-        # 🔴 THIS IS WHERE THE CHANGE GOES
-        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        model_path = os.path.join(BASE_DIR, "model", f"{model_name}.pkl")
+        MODEL_FILES = {
+            "Logistic Regression": "logistic_regression.pkl",
+            "Decision Tree": "decision_tree.pkl",
+            "KNN": "KNN.pkl",
+            "Naive Bayes": "naive_bayes.pkl",
+            "Random Forest": "random_forest.pkl",
+            "XGBoost": "XGBoost.pkl"
+        }
 
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        model_file = MODEL_FILES[model_name]
+        model_path = os.path.join(BASE_DIR, "model", model_file)        
         model = joblib.load(model_path)
+
+        # 🔴 THIS IS WHERE THE CHANGE GOES
+        #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        #model_path = os.path.join(BASE_DIR, "model", f"{model_name}.pkl")
+        #model = joblib.load(model_path)
+        
         y_pred = model.predict(X)
 
         st.subheader("Classification Report")
